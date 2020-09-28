3ec5e725a371de8ab1c661adde0c52a1fb688534
Sami <01@aa.aa>
2020-09-28 22:49:31 +0100

    02- Creating HeroSection and adding Home component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact>
-            <Navbar />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <Router>    
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />              
+            
+          </Switch>      
+      </Router>
+    </>
   );
 }
 
